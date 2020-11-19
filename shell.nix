1e11b74b15e64f0d28aaf0b193daa639e4798a89
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-11-19 19:10:37 +0100

    create a small nix-shell with all needed dependencies

---
+++ shell.nix
@@ -0,0 +1,16 @@
+let
+  pkgs = import <nixpkgs> {};
+in
+
+with pkgs;
+with stdenv;
+
+mkDerivation rec {
+  name = "magical-zilch-specification";
+  version = "0.0.1";
+
+  buildInputs = [
+    gnumake
+    texlive.combined.scheme-full
+  ];
+}
