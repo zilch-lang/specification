7b7311782d9c0d4b25f0b446f69e14f6af2867b8
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2021-11-25 09:50:51 +0100

    update to latest version of PyPi railroad-diagrams

--- syntax-diagrams.nix
+++ syntax-diagrams.nix
@@ -4,11 +4,11 @@ let
   railroad = with pkgs.python38Packages;
     buildPythonPackage rec {
       pname = "railroad-diagrams";
-      version = "1.1.0";
+      version = "1.1.1";
 
       src = fetchPypi {
         inherit pname version;
-        sha256 = "043ikgvgx45favixmvm97zl9q1xw4zfvblg8h83b5knhvryc12bp";
+        sha256 = "0xn9chq3nqyjj38f9akpc1cg31vryx0afjkrfq701qkbcqkw47la";
       };
     };
 in
